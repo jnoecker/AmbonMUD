7da87cc3f53cce987ab48c2af32058f2d7e66134
John Noecker Jr <jnoecker@gmail.com>
2026-01-25 06:10:48 -0500

    Linked larger demo zone into the world

--- src/main/kotlin/dev/ambon/domain/world/WorldFactory.kt
+++ src/main/kotlin/dev/ambon/domain/world/WorldFactory.kt
@@ -6,6 +6,7 @@ object WorldFactory {
     fun demoWorld(): World =
         WorldLoader.loadFromResources(
             listOf(
+                "world/demo.yaml",
                 "world/enchanted_forest.yaml",
                 "world/swamp.yaml",
             ),
