dd3d880dcf8f86a9f62ad26e605844f600d52351
John Noecker Jr. <jnoecker@gmail.com>
2026-01-25 04:40:46 -0500

    Support per-zone world namespaces and enforce normalized RoomIds. (#8)

--- src/main/kotlin/dev/ambon/domain/ids/RoomId.kt
+++ src/main/kotlin/dev/ambon/domain/ids/RoomId.kt
@@ -1,5 +1,18 @@
 package dev.ambon.domain.ids
 
-@JvmInline value class RoomId(
+@JvmInline
+value class RoomId(
     val value: String,
-)
+) {
+    init {
+        require(':' in value) {
+            "RoomId must be namespaced as <zone>:<room>, got '$value'"
+        }
+    }
+
+    val zone: String
+        get() = value.substringBefore(':')
+
+    val local: String
+        get() = value.substringAfter(':')
+}
