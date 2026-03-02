78a54fa35d0ba7f4fb07d77e5f8900bd682dea43
John Noecker Jr. <jnoecker@gmail.com>
2026-03-02 17:39:32 -0500

    refactor: introduce generic DefinitionRegistry base class (#360) (#382)

--- src/main/kotlin/dev/ambon/engine/status/StatusEffectRegistry.kt
+++ src/main/kotlin/dev/ambon/engine/status/StatusEffectRegistry.kt
@@ -1,15 +1,7 @@
 package dev.ambon.engine.status
 
-class StatusEffectRegistry {
-    private val definitions = mutableMapOf<StatusEffectId, StatusEffectDefinition>()
+import dev.ambon.engine.DefinitionRegistry
 
-    fun register(definition: StatusEffectDefinition) {
-        definitions[definition.id] = definition
-    }
-
-    fun get(id: StatusEffectId): StatusEffectDefinition? = definitions[id]
-
-    fun contains(id: StatusEffectId): Boolean = definitions.containsKey(id)
-
-    fun all(): Collection<StatusEffectDefinition> = definitions.values
+class StatusEffectRegistry : DefinitionRegistry<StatusEffectId, StatusEffectDefinition>({ it.id }) {
+    fun contains(id: StatusEffectId): Boolean = get(id) != null
 }
