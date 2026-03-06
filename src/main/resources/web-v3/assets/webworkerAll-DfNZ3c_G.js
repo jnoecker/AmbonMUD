352683c38fbc75d92fbe30031340d8f9cac52c8b
John Noecker Jr. <jnoecker@gmail.com>
2026-03-05 21:58:52 -0500

    Add dialogue speech bubble indicator and fix shop badge click (#575)
    - Replace dialogue role dot with dialog_indicator.png speech bubble
      sprite positioned above NPCs that have dialogue
    - Fix shop badge click: change hit area eventMode from "none" to "auto"
      so pointer events propagate to the container

--- src/main/resources/web-v3/assets/webworkerAll-DfNZ3c_G.js
+++ src/main/resources/web-v3/assets/webworkerAll-DfNZ3c_G.js
@@ -1,4 +1,4 @@
-import{G as R,b as S,s as b,M as P,a0 as I,Y as B,aa as T,T as _,ap as O,K as A,w as C,a9 as E,L as k}from"./index-D6Af1aCr.js";import{F as z}from"./Filter-60xKCFsx.js";var U=`in vec2 aPosition;
+import{G as R,b as S,s as b,M as P,a0 as I,Y as B,aa as T,T as _,ap as O,K as A,w as C,a9 as E,L as k}from"./index-y-Hatt9V.js";import{F as z}from"./Filter-CBMb8JjC.js";var U=`in vec2 aPosition;
 out vec2 vTextureCoord;
 
 uniform vec4 uInputSize;
