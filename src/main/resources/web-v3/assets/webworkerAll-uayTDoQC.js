055be999b7a2b5ea95da9f1a20634f10579fec79
John Noecker Jr <jnoecker@gmail.com>
2026-03-06 00:14:33 -0500

    Shrink command input bar to icon-sized, expand on focus
    The command input collapses to a small 36px send-icon button and
    smoothly expands when focused. Frees action bar space for vitals
    and ability slots.

--- src/main/resources/web-v3/assets/webworkerAll-uayTDoQC.js
+++ src/main/resources/web-v3/assets/webworkerAll-uayTDoQC.js
@@ -1,4 +1,4 @@
-import{G as R,b as S,s as b,M as P,a0 as I,Y as B,aa as T,T as _,ap as O,K as A,w as C,a9 as E,L as k}from"./index-DoBWZIQH.js";import{F as z}from"./Filter-CeEaLJul.js";var U=`in vec2 aPosition;
+import{G as R,b as S,s as b,M as P,a0 as I,Y as B,aa as T,T as _,ap as O,K as A,w as C,a9 as E,L as k}from"./index-DWdYbxOL.js";import{F as z}from"./Filter-BxZN1p-p.js";var U=`in vec2 aPosition;
 out vec2 vTextureCoord;
 
 uniform vec4 uInputSize;
